f660991fcb71b25086e847ac5da26334507c9de6
FakeStreamer_ <127014090+FakeStreamerTTV@users.noreply.github.com>
2024-03-08 12:08:40 +0100

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -2,13 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
     // Objecten om gegevens op te slaan
     const locationData = {};
     const temperatureData = {};
+    let lastLocationUpdateTime = 0;
 
     // Functie om de locatie op te halen
     function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.watchPosition(updateLocation);
-        } else {
-            document.getElementById("location").innerText = "Geolocation not supported in this browser.";
+        const currentTime = Date.now();
+        
+        // Controleer of het interval van 1 minuut is verstreken
+        if (currentTime - lastLocationUpdateTime >= 60000) {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    position => {
+                        updateLocation(position);
+                        lastLocationUpdateTime = currentTime; // Update de tijdstempel
+                    },
+                    error => console.error('Fout bij het ophalen van locatie:', error),
+                );
+            } else {
+                document.getElementById("location").innerText = "Geolocatie wordt niet ondersteund in deze browser.";
+            }
         }
     }
 
@@ -17,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
 
-        fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=YOUR_API_KEY`) //Google Maps API Key
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyB5JIk9Dm2n73UNxp_5zXE-57v7aDhb1b0`)
             .then(response => response.json())
             .then(data => {
                 const addressInfo = data.results[0];
@@ -26,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                     getCountryCode(city, countryCode => {
                         const locationText = `${city}, ${countryCode}`;
-                        document.getElementById("location").innerText = `Current location: ${locationText}`;
+                        document.getElementById("location").innerText = `Huidige locatie: ${locationText}`;
                         // Voeg locatie toe aan het object
                         locationData.location = locationText;
                         // Sla het JSON-object op in de lokale opslag
@@ -36,21 +48,21 @@ document.addEventListener("DOMContentLoaded", () => {
                         getTemperature(latitude, longitude);
                     });
                 } else {
-                    document.getElementById("location").innerText = "Location data not available.";
+                    document.getElementById("location").innerText = "Locatiegegevens niet beschikbaar.";
                 }
             })
-            .catch(error => console.error('Something went wrong parsing location data:', error));
+            .catch(error => console.error('Fout bij het ophalen van locatiegegevens:', error));
     }
 
     // Functie om de temperatuur op te halen
     function getTemperature(latitude, longitude) {
-        const apiKey = 'YOUR_API_KEY_OPENWEATHER'; //OpenWeatherMaps API Key
+        const apiKey = '54562ac676c8a848c27bde9fa94f56db';
 
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`)
             .then(response => response.json())
             .then(data => {
                 const temperature = Math.round(data.main.temp);
-                document.getElementById("temperature").innerText = `Current temperature: ${temperature}°C`;
+                document.getElementById("temperature").innerText = `Huidige temperatuur: ${temperature}°C`;
                 // Voeg temperatuur toe aan het object
                 temperatureData.temperature = temperature;
                 // Sla het JSON-object op in de lokale opslag
@@ -59,12 +71,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 // Stuur locatie- en temperatuurdata naar de server
                 sendToServer('/data', { locationData, temperatureData });
             })
-            .catch(error => console.error('Something went wrong pasring location data:', error));
+            .catch(error => console.error('Fout bij het ophalen van de temperatuur:', error));
     }
 
     function getCountryCode(city, callback) {
         // Functie om landcode op te halen
-        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=YOUR_API_KEY`) //Google Maps API Key
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyB5JIk9Dm2n73UNxp_5zXE-57v7aDhb1b0`)
             .then(response => response.json())
             .then(data => {
                 const addressInfo = data.results[0];
@@ -72,10 +84,10 @@ document.addEventListener("DOMContentLoaded", () => {
                     const countryCode = extractAddressComponent(addressInfo, 'country');
                     callback(countryCode);
                 } else {
-                    console.error('Invalid data from Google Geocoding API');
+                    console.error('Ongeldige gegevens ontvangen van Google Geocoding API');
                 }
             })
-            .catch(error => console.error('Something went wrong parsing Country Code:', error));
+            .catch(error => console.error('Fout bij het ophalen van landcode:', error));
     }
 
     function extractAddressComponent(addressInfo, componentType) {
@@ -83,12 +95,11 @@ document.addEventListener("DOMContentLoaded", () => {
         return component ? component.short_name : '';
     }
 
-    // Stel de update-interval in op 1 minuut 
     setInterval(() => {
         getLocation();
     }, 60000);
-
-    // Roep de functies aan
+    
+    // Roep de functie aan bij het laden van de pagina
     getLocation();
 });
 
@@ -103,7 +114,7 @@ function sendToServer(endpoint, data) {
     })
     .then(response => {
         if (!response.ok) {
-            throw new Error(`Something went wrong with sending data to the server: ${response.status} ${response.statusText}`);
+            throw new Error(`Fout bij het verzenden van gegevens naar de server: ${response.status} ${response.statusText}`);
         }
         return response.json();
     })
